2f905ac0d967800070d518a13757f109d00c3965
timi2506 <172171055+timi2506@users.noreply.github.com>
2026-04-06 00:24:49 +0200

    Add options flow for MyPayIndia configuration

--- mypayindia/config_flow.py
+++ mypayindia/config_flow.py
@@ -1,5 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
 from .const import DOMAIN
 
 class MyPayIndiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -16,3 +17,27 @@ class MyPayIndiaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         })
 
         return self.async_show_form(step_id="user", data_schema=schema)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return MyPayIndiaOptionsFlowHandler(config_entry)
+
+
+class MyPayIndiaOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            self.hass.config_entries.async_update_entry(
+                self.config_entry, data=user_input
+            )
+            return self.async_create_entry(title="", data={})
+
+        schema = vol.Schema({
+            vol.Required("username", default=self.config_entry.data.get("username")): str,
+            vol.Required("password", default=self.config_entry.data.get("password")): str,
+        })
+
+        return self.async_show_form(step_id="init", data_schema=schema)
